5ca84b632824e604bf97cd52e12b5258d3418b2e
John Wieland <johnny.c.sparkles@gmail.com>
2018-10-25 20:39:02 +1000

    Got the spies setup using sinon and chai and got the private header actually working

--- tests/ui/PrivateHeader.test.js
+++ tests/ui/PrivateHeader.test.js
@@ -1,25 +1,39 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import expect from 'expect';
+import chai from 'chai';
+import sinon from 'sinon';
 import { mount } from 'enzyme';
 import PrivateHeader from './../../imports/ui/PrivateHeader';
 
 if (Meteor.isClient) {
   describe('PrivateHeader', function() {
     it('should set button text to logout', function() {
-      const wrapper = mount(<PrivateHeader/>);
+      const wrapper = mount(<PrivateHeader title="Test title" handleLogout={() => {}}/>);
 
-      expect(wrapper.find('button').text()).toBe('Logout');
+      chai.expect(wrapper.find('button').text()).to.equal('Logout');
     });
 
     it('should set the title', function() {
       const title = "This is the title";
-      const wrapper = mount(<PrivateHeader title={title}/>);
+      const wrapper = mount(<PrivateHeader title={title} handleLogout={() => {}}/>);
 
-      expect(wrapper.find('h1').text()).toBe(title);
+      chai.expect(wrapper.find('h1').text()).to.equal(title);
     });
 
     it('should call the function', function() {
+      const spy =  sinon.fake();
+      spy(1, 2, 3);
+      chai.expect(spy.called).to.equal(true);
+    });
+
+    it('should call handle logout on click', function() {
+      const spy = sinon.fake();
+      const wrapper = mount(<PrivateHeader title="Title" handleLogout={spy} />);
+      
+      wrapper.find('button').simulate('click');      
+
+      chai.expect(spy.called).to.equal(true);
+      chai.expect(spy.getCalls().length).to.equal(1);
     });
   });
 }
