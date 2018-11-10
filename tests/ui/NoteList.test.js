fec6130e1f2f7a0efb7ca78420aff6207a18b5e2
John Wieland <johnny.c.sparkles@gmail.com>
2018-11-11 08:39:35 +1000

    Added id's to the notes test data

--- tests/ui/NoteList.test.js
+++ tests/ui/NoteList.test.js
@@ -13,12 +13,14 @@ if (Meteor.isClient) {
   describe('NoteList', () => {
     const notes = [
       {
+        _id: 'noteId1',
         title: "Note1",
         body: "Body1",
         userId: "UserId1",
         updatedAt: moment().valueOf()
       },
       {
+        _id: 'noteId2',
         title: "Note2",
         body: "Body2",
         userId: "UserId2",
