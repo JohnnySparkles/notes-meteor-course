38551f0981954dc1651742c480f56a08e9f3b099
John Wieland <johnny.c.sparkles@gmail.com>
2018-11-11 08:48:44 +1000

    Refactored the NoteListItem.test.js to use the fixtures

--- tests/ui/NoteListItem.test.js
+++ tests/ui/NoteListItem.test.js
@@ -5,21 +5,16 @@ import sinon from 'sinon';
 import {mountRoute } from '../utils/router-wrapper';
 import { NoteListItem } from '../../imports/ui/NoteListItem';
 import moment from 'moment';
+import { notes } from '../fixtures/fixtures';
 
 
 if ( Meteor.isClient) {
   describe('NoteListItem', () => {
     it('should display the title and updatedAt', () => {
-      const note = {
-        title: "This is the title",
-        body: "This is the body",
-        userId: "AUserID1",
-        updatedAt: moment().valueOf()
-      }
-      const wrapper = mountRoute(<NoteListItem note={note}/>);
+      const wrapper = mountRoute(<NoteListItem note={notes[0]}/>);
 
-      expect(wrapper.find('h5').text()).to.be.equal(note.title);
-      expect(wrapper.find('p').text()).to.be.equal(moment(note.updatedAt).format('M/DD/YYYY'));
+      expect(wrapper.find('h5').text()).to.be.equal(notes[0].title);
+      expect(wrapper.find('p').text()).to.be.equal(moment(notes[0].updatedAt).format('M/DD/YYYY'));
     });
 
     it('should display untitled note if there is not a title', () => {
